b4c03a93f07abb00393ce6ed3d88b901f3866e4c
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-09 15:56:17 +0100

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,47 @@
+
+const myAge = 25; 
+    if (myAge < 18) {
+        console.log('Sorry, you are too young');
+        
+    } else if (myAge >= 18 && myAge <= 25) {
+        console.log("You get 50% off!");
+    
+    } else if (myAge > 25) 
+    console.log('Welcome and cheers, but no discount');
+
+
+const firstName = 'Bram'; 
+    if (firstName == 'Sarah' || firstName == 'Bram') {
+        console.log('Free beer for ya!');  
+    } 
+
+const totalAmount = 10;
+    if (totalAmount >= 25 && totalAmount < 50 ) {
+        console.log('Free bitterballen!');  
+    }
+    else if (totalAmount >= 50 && totalAmount < 100 ) {
+        console.log('Free nachos!');  
+    }
+    else if (totalAmount >= 100) {
+        console.log('Free champagne!');  
+    }
+    else {
+        console.log('spend at least 25 euros for free stuff');
+    }
+
+const isFemale = true;
+    if (isFemale == true)  {
+        console.log('Please come in mam');
+    }  else {
+        console.log('Sorry Sir, Ladies only');
+    }
+
+
+const driverStatus = 'bob';
+    if (driverStatus == 'bob')  {
+        console.log('You are allowed to drive');
+    }  else {
+        console.log('Sorry, you are pissed');
+    }
+
+ 
